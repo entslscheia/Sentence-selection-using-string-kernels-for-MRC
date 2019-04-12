c7482f8bbea737426815a3cf9d7c85d7b667e2b6
entslscheia <yugu772@yahoo.com>
2019-04-12 21:21:31 +0800

    generate unanswerable questions

--- split_train.py
+++ split_train.py
@@ -1,15 +1,17 @@
-# This file aims at split train-v1.1.json into multiple smaller files
+# This file aims at split train.json into multiple smaller files
 import json
 
-with open('data/SQuAD1.1/train-v1.1.json') as f:
+with open('data/SQuAD/train-v2.0.json') as f:
     train = json.load(f)
 
 num = len(train['data'])
+print(num)
 num_i = int(num / 10)
 
 for i in range(10):
-    with open('data/SQuAD1.1/splited/' + str(i) + '.json', 'w') as outfile:
+    with open('data/SQuAD/splited/' + str(i) + '.json', 'w') as outfile:
         data = {}
+        data['version'] = '2.0'
         data['data'] = []
         if i != 9:
             data['data'].extend(train['data'][i * num_i : (i + 1) * num_i])
--- test.py
+++ test.py
@@ -1,6 +1,26 @@
 import json
 
-with open('./data/SQuAD1.1/dev-v1.1.txt', 'r') as file:
+# for visualization
+with open('data/SQuAD/splited/toy.json', 'r') as file:
     data = file.read()
 obj = json.loads(data)
-json.dump(obj, open('./data/SQuAD1.1/dev-v1.1.txt', 'w'), indent=2)
+json.dump(obj, open('data/SQuAD/splited/toy.json', 'w'), indent=2)
+
+
+# for generating toy example data
+# with open('data/SQuAD1.1/train-v1.1.json') as f:
+#     train = json.load(f)
+#
+# with open('data/SQuAD1.1/splited/toy.json', 'w') as outfile:
+#     data = {}
+#     data['data'] = []
+#     data0 = {}
+#     data0['title'] = train['data'][0]['title']
+#     data0['paragraphs'] = []
+#     data1 = {}
+#     data1['context'] = train['data'][0]['paragraphs'][0]['context']
+#     data1['qas'] = []
+#     data1['qas'].extend(train['data'][0]['paragraphs'][0]['qas'][0:2])
+#     data0['paragraphs'].append(data1)
+#     data['data'].append(data0)
+#     json.dump(data, outfile)
